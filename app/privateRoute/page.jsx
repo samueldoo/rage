9d7687e687867aff834802ca1f40122b2bdb9d32
Alademehin Samuel <133213017+samueldoo@users.noreply.github.com>
2024-04-22 05:43:20 +0100

    changes

--- app/privateRoute/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom'; // Import for navigation
-
-const PrivateRoute = ({ children, isLoggedIn }) => {
-  return isLoggedIn ? children : <Navigate to="/login" replace />; // Redirect to login
-};
-
-export default PrivateRoute;
